372223d7bd54e5a85ba2e1643148442cd6a0acc7
demon-ora <softwareselling26@gmail.com>
2023-03-29 18:34:31 +0545

    download language translate libary using composer and worte php code to translet langugae and also sove error which was cause due to missing of mbstring which cause lower verison of libary to download

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
-  var lang_one = $("#lang_one").val();
-  var lang_two = $("#lang_two").val();
-  var text = $("#text").val();
+  $("#convert").on("click", function () {
+    var lang_one = $("#lang_one").val();
+    var lang_two = $("#lang_two").val();
+    var text = $("#text").val();
 
-  $.ajax({
-    url: "process.php",
-    type: "post",
-    data: { lang_one: lang_one, lang_two: lang_two, text: text },
-    success: function (status) {
-      console.log(status);
-    },
+    $.ajax({
+      url: "process.php",
+      type: "post",
+      data: { lang_onee: lang_one, lang_twoo: lang_two, textt: text },
+      success: function (status) {
+        console.log(status);
+      },
+    });
   });
 });
--- process.php
+++ process.php
@@ -1,1 +1,28 @@
-hello 
+<?php  
+
+require_once "vendor/autoload.php";
+
+use Stichoza\GoogleTranslate\GoogleTranslate;
+
+$lang_one = $_POST['lang_onee'];
+$lang_two = $_POST['lang_twoo'];
+$text = $_POST['textt'];
+
+
+
+if($lang_one == "AUTO_DETECT"){
+    // echo $text;
+$tr = new GoogleTranslate($lang_two);
+
+
+$text = $tr->translate($text);
+$lang_one = $tr->getLastDetectedSource();
+
+echo GoogleTranslate::trans($text, $lang_two, $lang_one);
+
+}else{
+    echo GoogleTranslate::trans($text, $lang_two, $lang_one);
+}
+
+
+?>
