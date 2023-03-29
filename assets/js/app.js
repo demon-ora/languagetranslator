f901145c7fac8cf9513500ddba1e7cd644f563c7
demon-ora <softwareselling26@gmail.com>
2023-03-29 17:21:24 +0545

    wrote jquery code in app js to send data to process.php

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
- 
+$(document).ready(function () {
+  var lang_one = $("#lang_one").val();
+  var lang_two = $("#lang_two").val();
+  var text = $("#text").val();
+
+  $.ajax({
+    url: "process.php",
+    type: "post",
+    data: { lang_one: lang_one, lang_two: lang_two, text: text },
+    success: function (status) {
+      console.log(status);
+    },
+  });
+});
